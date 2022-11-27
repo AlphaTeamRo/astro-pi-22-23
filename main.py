9580af8d54600e4ad63ad6490cc7b40c08da331e
CristianEduardMihai <cristianeduardmihai@gmail.com>
2022-11-27 20:31:07 +0200

    added coords, moved pixelcount back to main file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
 from functions.fastiecm import fastiecm
-from functions.pixelcount import pixelcount
+
 from functions.file_checker import files_check
 
 from orbit import ISS
 from picamera import PiCamera
-
+from matplotlib import colors
+from scipy.spatial import cKDTree as KDTree
 from PIL import Image
 from pathlib import Path
 from datetime import datetime, timedelta
@@ -24,7 +25,7 @@ def create_csv(data_file):
     with open(data_file, 'w') as f:
         try:
             writer = csv.writer(f)
-            header = ("Date/time", "Country", "City", "Weather")
+            header = ("Image", "Healthy", "Declining", "Unhealthy", "Location")
             writer.writerow(header)
         except:
             logger.error("Couldn't create a csv file")
@@ -36,6 +37,44 @@ def add_csv_data(data_file, data):
             writer.writerow(data)
         except:
             logger.error("Couldn't add csv data")
+
+#calculate the number of pixels in the image
+def pixelcount(img):
+    #save the image(temporary, for testing)
+    #cv2.imwrite('ndvi.png', img)
+    img = Image.open(img)
+    #borrow a list of named colors from matplotlib
+    use_colors = {k: colors.cnames[k] for k in ['red', 'green', 'yellow', 'purple']}
+
+    #translate hexstring to RGB tuple
+    named_colors = {k: tuple(map(int, (v[1:3], v[3:5], v[5:7]), 3*(16,)))
+                    for k, v in use_colors.items()}
+    ncol = len(named_colors)
+
+    ncol -= 1
+    no_match = named_colors.pop('purple')
+
+    #make an array containing the RGB values 
+    color_tuples = list(named_colors.values())
+    color_tuples.append(no_match)
+    color_tuples = np.array(color_tuples)
+    color_names = list(named_colors)
+    color_names.append('no match')
+
+    #start the actual counting
+    #build tree
+    tree = KDTree(color_tuples[:-1])
+    #tolerance for color match `inf` means use best match no matter how bad it may be
+    tolerance = np.inf
+    #find closest color in tree for each pixel in picture
+    dist, idx = tree.query(img, distance_upper_bound=tolerance)
+    #count and reattach names
+    counts = dict(zip(color_names, np.bincount(idx.ravel(), None, ncol+1)))
+
+    logger.info(counts)
+
+    return counts
+
 
 def convert(angle):
     """
@@ -66,6 +105,8 @@ def capture(camera, image):
 
     # Capture the image
     camera.capture(image)
+
+    return point
 
 #function for increasing the contrast of the image
 def contrast_stretch(im):
@@ -114,7 +155,7 @@ while (now_time < project_start_time + timedelta(minutes=170)):
 
     #-----------------TAKE CAMERA PICTURE AND STORE IT IN image-----------------#
     image_file = f"{base_folder}/raw/{timestamp}.jpg"
-    capture(cam, image_file)
+    location = capture(cam, image_file)
 
     image = cv2.imread(image_file)
     #image = cv2.imread('park.png')
@@ -131,7 +172,23 @@ while (now_time < project_start_time + timedelta(minutes=170)):
     color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
     cv2.imwrite('color_mapped_image.png', color_mapped_image)
 
-    pixelcount('color_mapped_image.png', logger)
+    counts = pixelcount('color_mapped_image.png', logger)
+
+    #calculate the heath index
+    #green = healthy
+    #yellow = declining
+    #red = unhealthy
+    allcounts = counts["red"] + counts["green"] + counts["yellow"]
+    healthy = round((counts["green"] / allcounts) * 100)
+    declining = round((counts["yellow"] / allcounts) * 100)
+    unhealthy = round((counts["red"] / allcounts) * 100)
+
+    logger.info("Healthy: " + str(healthy))
+    logger.info("Declining: " + str(declining))
+    logger.info("Unhealthy: " + str(unhealthy))
+
+    row = (timestamp, str(healthy), str(declining), str(unhealthy), str(location))
+    add_csv_data(data_file, row)
 
 
     try:
