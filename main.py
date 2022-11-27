510d9997190c576a4996230fbb945855c63cfc1c
CristianEduardMihai <cristianeduardmihai@gmail.com>
2022-11-27 15:00:53 +0200

    added the first attempt of NDVI color counting

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ import cv2
 import numpy as np
 from fastiecm import fastiecm
 
-original = cv2.imread('park.png')
+from matplotlib import colors
+from scipy.spatial import cKDTree as KDTree
+from PIL import Image
+from pathlib import Path
+from datetime import datetime, timedelta
+import re
 
-#temp function for displaying resulted images
-def display(image, image_name):
-    image = np.array(image, dtype=float)/float(255)
-    shape = image.shape
-    height = int(shape[0] / 2)
-    width = int(shape[1] / 2)
-    image = cv2.resize(image, (width, height))
-    cv2.namedWindow(image_name)
-    cv2.imshow(image_name, image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+base_folder = Path(__file__).parent.resolve()
+
+caming = cv2.imread('park.png')
 
 #function for increasing the contrast of the image
 def contrast_stretch(im):
@@ -38,24 +35,72 @@ def calc_ndvi(image):
     ndvi = (b.astype(float) - r) / bottom
     return ndvi
 
-#display the original and the contrasted image
-display(original, 'Original')
-contrasted = contrast_stretch(original)
-display(contrasted, 'Contrasted original')
-#save the contrasted image
-cv2.imwrite('contrasted.png', contrasted)
-#calculate ndvi
+#-----------------LOOP START-----------------#
+
+#timestamp format: yyyy-mm-dd_hh-mm-ss
+timestamp = str(datetime.now())
+timestamp = timestamp[0:19]
+timestamp = re.sub(r'[:]', '-', re.sub(r'[ ]', '_', timestamp))
+
+#save the original image
+cv2.imwrite(f'{base_folder}/raw/{timestamp}.png', caming)
+
+#PROCESS THE IMAGE TO GET THE NDVI DATA
+contrasted = contrast_stretch(caming)
 ndvi = calc_ndvi(contrasted)
-display(ndvi, 'NDVI')
-cv2.imwrite('ndvi.png', ndvi)
-
-#contrast ndvi again (might be temporary)
 ndvi_contrasted = contrast_stretch(ndvi)
-display(ndvi_contrasted, 'NDVI Contrasted')
-cv2.imwrite('ndvi_contrasted.png', ndvi_contrasted)
-
 #add color mapping(could be removed later, if we work using shades of grey instead of colors)
 color_mapped_prep = ndvi_contrasted.astype(np.uint8)
 color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
-display(color_mapped_image, 'Color mapped')
 cv2.imwrite('color_mapped_image.png', color_mapped_image)
+
+#calculate the number of pixels in the image
+def pixelcount(img):
+    #save the image(temporary, for testing)
+    #cv2.imwrite('ndvi.png', img)
+    img = Image.open(img)
+    #borrow a list of named colors from matplotlib
+    use_colors = {k: colors.cnames[k] for k in ['red', 'green', 'yellow', 'purple']}
+    
+    #translate hexstring to RGB tuple
+    named_colors = {k: tuple(map(int, (v[1:3], v[3:5], v[5:7]), 3*(16,)))
+                    for k, v in use_colors.items()}
+    ncol = len(named_colors)
+    
+    ncol -= 1
+    no_match = named_colors.pop('purple')
+    
+    #make an array containing the RGB values 
+    color_tuples = list(named_colors.values())
+    color_tuples.append(no_match)
+    color_tuples = np.array(color_tuples)
+    color_names = list(named_colors)
+    color_names.append('no match')
+
+    #start the actual counting
+    #build tree
+    tree = KDTree(color_tuples[:-1])
+    #tolerance for color match `inf` means use best match no matter how bad it may be
+    tolerance = np.inf
+    #find closest color in tree for each pixel in picture
+    dist, idx = tree.query(img, distance_upper_bound=tolerance)
+    #count and reattach names
+    counts = dict(zip(color_names, np.bincount(idx.ravel(), None, ncol+1)))
+
+    print(counts)
+
+    #calculate the heath index
+    #green = healthy
+    #yellow = moderate
+    #red = unhealthy
+    allcounts = counts["red"] + counts["green"] + counts["yellow"]
+    healthy = round((counts["green"] / allcounts) * 100)
+    moderate = round((counts["yellow"] / allcounts) * 100)
+    unhealthy = round((counts["red"] / allcounts) * 100)
+
+    print("Healthy: " + str(healthy))
+    print("Moderate: " + str(moderate))
+    print("Unhealthy: " + str(unhealthy))
+    
+
+pixelcount('color_mapped_image.png')
